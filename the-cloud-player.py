5a632f88190680d8bf186fed1cb616d68350003d
Henrik Berggren <hinke@hinkeb.com>
2008-11-24 00:02:13 +0100

    Added versioning for playlists and  made a first draft for the app page

--- the-cloud-player.py
+++ the-cloud-player.py
@@ -20,34 +20,32 @@ import utils
 class StartPage(webapp.RequestHandler):
   def get(self):
     self.response.out.write(template.render('index.html', {}))
+
     
 class PlayerPage(webapp.RequestHandler):
   def get(self):
     app_user = utils.get_current_user()
     if users.get_current_user() and not app_user:
-      app_user = models.User(google_user=users.get_current_user())
-      app_user.put()
+      app_user = utils.init_new_user()
     
-    self.response.out.write(template.render('player.html', {'user':users.get_current_user(),'rando': random.random(), 'login_url': users.create_login_url("/app"), 'logout_url':users.create_logout_url("/")}))
+    self.response.out.write(template.render('player.html', {'user':app_user,'rando': random.random(), 'login_url': users.create_login_url("/app"), 'logout_url':users.create_logout_url("/")}))
 
 class SharePlaylist(webapp.RequestHandler):
   def get(self):
     if not users.get_current_user():
       self.redirect(users.create_login_url(self.request.uri)) 
-          
-    app_user = utils.get_current_user()
-    if users.get_current_user() and not app_user:
-      app_user = models.User(google_user=users.get_current_user())
-      app_user.put()
-          
-    key = utils.url_to_playlist_key(self.request.uri)
-    playlist = db.get(db.Key(key))
-    
-    if not app_user.has_playlist(playlist):
-      library_item = models.Library(user=app_user, playlist=playlist, is_owner=False)
-      library_item.put()
-    
-    self.redirect("/app")  
+    else:      
+      app_user = utils.get_current_user()
+      if users.get_current_user() and not app_user:
+        app_user = utils.init_new_user()
+        
+      key = utils.url_to_playlist_key(self.request.uri)
+      playlist = db.get(db.Key(key))
+      if not app_user.has_playlist(playlist):
+        library_item = models.Library(user=app_user, playlist=playlist, is_owner=False)
+        library_item.put()
+  
+      self.redirect("/app?flash=add_shared_playlist")  
 
 
 class Playlist(webapp.RequestHandler):  
@@ -58,7 +56,6 @@ class Playlist(webapp.RequestHandler):
     self.response.out.write(library_item.serialize())
 
   def post(self):
-    #try:
     method = self.request.get("_method")
     key = utils.url_to_playlist_key(self.request.uri)
     playlist = db.get(db.Key(key))
@@ -71,16 +68,46 @@ class Playlist(webapp.RequestHandler):
       if not library_item:
         library_item = models.Library(user=current_user, playlist=playlist, is_owner=False)
         library_item.put()
-
-      if(self.request.get('name')):
-        playlist.name = self.request.get('name')
-      if(self.request.get('tracks')):
-        playlist.tracks = self.request.get('tracks')
-      if(self.request.get('position')):
-        library_item.position = self.request.get('position')
-    
-      playlist.put()        
-  
+      
+      if(int(self.request.get('version')) == playlist.version):
+        if(self.request.get('name')):
+          playlist.name = self.request.get('name')
+        if(self.request.get('tracks')):
+          playlist.tracks = self.request.get('tracks')
+        if(self.request.get('position')):
+          library_item.position = int(self.request.get('position'))
+        if(self.request.get('collaborative')):
+          playlist.collaborative = bool(self.request.get('collaborative'))
+        
+        #smart filters  
+        if(self.request.get('genre')):
+          playlist.genre = self.request.get('genre')
+        if(self.request.get('tags')):
+          playlist.tags = self.request.get('tags')
+        if(self.request.get('uploaded_from')):
+          playlist.uploaded_from = self.request.get('uploaded_from')
+        if(self.request.get('uploaded_to')):
+          playlist.uploaded_to = self.request.get('uploaded_to')
+        if(self.request.get('bpm_from')):
+          playlist.bpm_from = int(self.request.get('bpm_from'))
+        if(self.request.get('bpm_to')):
+          playlist.bpm_to = int(self.request.get('bpm_to'))
+        if(self.request.get('search_term')):
+          playlist.search_term = self.request.get('search_term')
+        if(self.request.get('user_favorites')):
+          playlist.user_favorites = self.request.get('user_favorites')
+        if(self.request.get('order')):
+          playlist.order = self.request.get('order')
+        if(self.request.get('duration_from')):
+          playlist.duration_from = int(self.request.get('duration_from'))
+        if(self.request.get('duration_to')):
+          playlist.duration_to = int(self.request.get('duration_to'))
+      
+        playlist.version += 1
+        playlist.put()        
+      else:
+        self.response.out.write(playlist.serialize())
+        
     elif method == "DELETE":
       if library_item.is_owner and not playlist.collaborative:
         users_connected_to_playlist = playlist.users()
@@ -91,30 +118,24 @@ class Playlist(webapp.RequestHandler):
         
       elif playlist.collaborative:
         library_item.delete()
-        if len(playlist.users()) == 0:
+        if not playlist.has_user():
           playlist.delete()
           
       elif not library_item.is_owner:
         library_item.delete()
-
-    #self.response.set_status(200)
-    #except StandardError:
-      #self.error(500)      
       
 class Playlists(webapp.RequestHandler):
   def get(self):
     self.response.out.write(utils.serialize_library(utils.get_current_user().playlists()))
 
   def post(self):  #Create new playlist
-    playlist = models.Playlist(name = self.request.get("name"))
+    playlist = models.Playlist(name = self.request.get("name"), smart = bool(self.request.get("smart")))
         
     playlist.put()
     library_item = models.Library(user=utils.get_current_user(), playlist=playlist, is_owner=True, position = int(self.request.get("position")))
     library_item.put()
     
     self.response.out.write(playlist.serialize())
-
-
 
 def main():
   application = webapp.WSGIApplication([
@@ -123,7 +144,8 @@ def main():
                                       ('/playlists/.*', Playlist), 
                                       ('/share/.*', SharePlaylist), 
                                       ('/', StartPage), 
-                                      ('/app', PlayerPage)
+                                      ('/app', PlayerPage),
+                                      ('/app/', PlayerPage)
                                       ], debug=True)
   run_wsgi_app(application)
 
