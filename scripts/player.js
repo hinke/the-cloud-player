6a4902a92c152e4ced8fc3a203b88d35a50936e2
Eric Wahlforss <eric@Forss.(none)>
2009-07-10 15:06:10 +0200

    fixing bug in hotness filtering of artist tracks

--- scripts/player.js
+++ scripts/player.js
@@ -620,7 +620,8 @@ SC.Player.prototype = {
               smart: true,
               smart_filter: {
                 artist : track.user.permalink,                
-                order: "hotness"
+                order: "hotness",
+                hotness_from : "2007-01-01"
               },
               dontPersist : true,
               dontShowPlaylistItem : true
